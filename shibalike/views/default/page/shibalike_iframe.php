f8b8a03b4ebb6869803fa2821c2df6fcbb5f1195
Steve Clay <steve@mrclay.org>
2011-08-11 11:42:40 -0400

    frame fixes for display inside our home page

--- shibalike/views/default/page/shibalike_iframe.php
+++ shibalike/views/default/page/shibalike_iframe.php
@@ -15,6 +15,25 @@ header("Content-type: text/html; charset=UTF-8");
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <?php echo elgg_view('page/elements/head', $vars); ?>
+<style>
+body {
+    padding:0;
+    margin: 0 10px 0;
+}
+.elgg-page-default {
+    width:auto; 
+    min-width:0;
+}
+.elgg-page-default .elgg-page-body > .elgg-inner {
+    width: auto;
+    margin: 0;
+}
+.elgg-page-body {
+    background:#fff;
+    margin:0;
+    padding: 0;
+}
+</style>
 </head>
 <body>
 <div class="elgg-page elgg-page-default">
@@ -27,5 +46,11 @@ header("Content-type: text/html; charset=UTF-8");
 		</div>
 	</div>
 </div>
+<script>
+$(function () {
+    $('form, a').attr('target', '_top');
+    $('input[name=persistent]').parent().remove();
+});
+</script>
 </body>
 </html>
